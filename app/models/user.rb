514a230d2ceb5831537d7aff30ebf0bf1685ec4c
jnsrjl <jnsrjl@gmail.com>
2016-08-25 22:52:57 +0000

    Add simple validations for name, email and password
    Name: between 3-30 chars. Email: max 100 chars, regexed and unique. Password: min 8 chars.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ActiveRecord::Base
+  # Simple ruby email regex from emailregex.com
+  REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+  # Validations
+  # Name: present and between 3 - 30 chars
+  validates :name, presence: true, length: { minimum: 3, maximum: 30 }
+
+  # Email: present, max 100 chars, regexed and unique
+  validates :email, presence: true, length: { maximum: 100 },
+    format: { with: REGEX }, uniqueness: { case_sensitive: false }
+
+  # Activate secure password functionality
+  has_secure_password
+
+  # Password: min 8 chars
+  validates :password, length: { minimum: 8 }
 end
