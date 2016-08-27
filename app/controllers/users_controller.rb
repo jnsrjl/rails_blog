a55b3501aab3c4c3ce78ce8a351e53196ce5f190
jnsrjl <jnsrjl@gmail.com>
2016-08-27 17:30:28 +0000

    Save validated registers to database; Add simple user profile page
    Silence huge backtraces.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-
+    @user = User.find(params[:id])
   end
 
   # Register form needs a user variable
@@ -13,18 +13,22 @@ class UsersController < ApplicationController
     # Use private method to strongify parameters
     @user = User.new(user_strong_params)
 
+    # User parameters passed validations
     if @user.save
-      #TODO
+      # TEMP : Redirect to profile page for now : later to blog front
+      redirect_to @user
+
+    # Some parameters didn't pass
     else
+      # Render new with error messages attached to @user
       render 'new'
     end
   end
 
 
-  ## Private area
-
   private
 
+  # Protect against form parameter forgery
   # Returns only required and permitted parameters
   def user_strong_params
     params.require(:user).permit(:name, :email, :password,
