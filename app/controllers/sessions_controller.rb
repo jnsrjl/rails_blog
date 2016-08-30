430daaceaa4ca004509a6ee2d237b5c130728eae
jnsrjl <jnsrjl@gmail.com>
2016-08-30 23:26:46 +0000

    Update login view; Create create-action for sessions
    Create action is incomplete - needs cookies. Link to login added on site template.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,28 @@
 class SessionsController < ApplicationController
+
   def new
+
+  end
+
+  # Create a new session
+  def create
+    # Get submitted email and password
+    address = params[:session][:email]
+    password = params[:session][:password]
+
+    # Pull the user from the db
+    user = User.find_by(email: address.downcase)
+
+    # User exists and password is correct
+    if user && user.authenticate(password)
+
+    # There was invalid data
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+
   end
 end
