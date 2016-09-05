8b57c46dd8cc89b566449eba34d0cb9c814bb790
jnsrjl <jnsrjl@gmail.com>
2016-09-05 00:27:35 +0000

    Add access control for user edits
    Add before_actions to determine if user is logged in and correct in relation to page trying to access. Define two helper methods to check for those conditions.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  # Check session status if user tries to access user edit or update -actions
+  before_action :check_login_status, only: [:edit, :update]
+
+  # Also check that the user is correct one
+  before_action :check_user_correct, only: [:edit, :update]
+
   # Show user profile page based on id
   def show
     @user = User.find(params[:id])
@@ -57,13 +63,38 @@ class UsersController < ApplicationController
     end
   end
 
+##################################PRIVATES######################################
 
   private
 
-  # Protect against form parameter forgery
-  # Returns only required and permitted parameters
-  def user_strong_params
-    params.require(:user).permit(:name, :email, :password,
-      :password_confirmation)
-  end
+    # Protect against form mass assignment
+    # Returns only required and permitted parameters
+    def user_strong_params
+      params.require(:user).permit(:name, :email, :password,
+        :password_confirmation)
+    end
+
+    # Returns true, if user has created a session
+    def check_login_status
+      # If user hasn't logged in
+      unless logged_in_user?
+        # Show reminder message
+        flash[:info] = "Log in to continue."
+
+        # Redirect user to login page
+        redirect_to login_url
+      end
+    end
+
+    # Returns true, if user is the same as the user being accessed
+    def check_user_correct
+      # Fetch user by id
+      @user = User.find(params[:id])
+
+      # Redirect to home page if wrong user
+      unless @user == logged_in_user
+        redirect_to root_url
+      end
+
+    end
 end
